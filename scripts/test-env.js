ba71578ac0bd65910d980c8308e3d0af1eb68781
manoel-domingos <manoeldomingos2@gmail.com>
2026-05-30 20:34:11 -0400

    chore: remove .canny-counter.json configuration file

--- scripts/test-env.js
+++
@@ -1,4 +0,0 @@
-console.log("Available environment variables:");
-for (const key of Object.keys(process.env)) {
-  console.log(`- ${key}: ${process.env[key] ? 'DEFINED' : 'UNDEFINED'} (length: ${process.env[key]?.length || 0})`);
-}
